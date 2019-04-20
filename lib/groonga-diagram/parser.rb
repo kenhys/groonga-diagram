b71b4e4436108526ef70a9dbb443e9972055034d
Kentaro Hayashi <kenhys@gmail.com>
2019-04-20 20:38:06 +0900

    Parse .expected

--- lib/groonga-diagram/parser.rb
+++ lib/groonga-diagram/parser.rb
@@ -21,9 +21,80 @@ module GroongaDiagram
 
     class GrntestExpectedParser < self
       def initialize
+        @parser = Groonga::Command::Parser.new
       end
 
-      def parse
+      def parse(input)
+        @parser.on_command do |command|
+          #p command
+        end
+        @parser.on_load_start do |command|
+          @data = []
+          @columns = []
+        end
+        @parser.on_load_columns do |command, columns|
+          p columns
+        end
+        @parser.on_load_value do |command, value|
+          value.keys.each do |column|
+            unless @columns.include?(column)
+              @columns << column
+            end
+          end
+          @data << value
+        end
+        @parser.on_load_complete do |command|
+          puts command.arguments[:table]
+          rows = []
+          @data.each do |data|
+            row = []
+            @columns.each do |column|
+              if data.has_key?(column)
+                row << data[column]
+              else
+                row << "-"
+              end
+            end
+            rows << row
+          end
+          table = TTY::Table.new header: @columns, rows: rows
+          renderer = TTY::Table::Renderer::Unicode.new(table)
+          puts renderer.render
+        end
+        in_response = false
+        in_load = false
+        response = ""
+        input.each_line do |line|
+          if line =~ /\A\[\[/
+          elsif line =~ /\Aload/
+            in_load = true
+            @parser << line
+          elsif line =~ /\A\[/
+            if in_load
+              @parser << line
+            else
+              in_response = true
+              response = "[\n"
+            end
+          elsif line =~ /\A\]/
+            if in_response
+              response << "]"
+              puts response
+            end
+            if in_load
+              @parser << line
+            end
+            in_load = false if in_load
+            in_response = false if in_response
+          else
+            if in_response
+              response << line
+            else
+              @parser << line
+            end
+          end
+        end
+        @parser.finish
       end
     end
 
