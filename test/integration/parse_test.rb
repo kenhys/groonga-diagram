90f1f94ace7bc723d985e51075e526af38f5435b
Kentaro Hayashi <kenhys@gmail.com>
2019-04-27 00:22:33 +0900

    Fix path

--- test/integration/parse_test.rb
+++ test/integration/parse_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
-require 'groonga-diagram/commands/parse'
+require 'groonga-diagram/command/parser'
 require 'groonga-diagram/parser'
 
-class GroongaDiagram::Commands::ParseTest < Test::Unit::TestCase
+class GroongaDiagram::Command::ParseTest < Test::Unit::TestCase
   def test_executes_groonga_diagram_help_parse_command_successfully
     output = `groonga-diagram help parse`
     expected_output = <<-OUT
