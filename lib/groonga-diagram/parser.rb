ee561ebc48c86fa378042660ec608b99ac1ed429
Kentaro Hayashi <kenhys@gmail.com>
2019-04-26 23:39:55 +0900

    Drop needless code

--- lib/groonga-diagram/parser.rb
+++ lib/groonga-diagram/parser.rb
@@ -5,27 +5,3 @@ require "json"
 
 require "groonga-diagram/parser/test"
 require "groonga-diagram/parser/expected"
-
-module GroongaDiagram
-  module Parser
-    class GrnParser
-      def initialize(options={})
-        @options = options
-        @output = options[:output] || $stdout
-        case @options[:format]
-        when "test"
-          @parser = GrntestParser.new({:output => @output})
-        when "expected"
-          @parser = GrntestExpectedParser.new({:output => @output})
-        else
-          raise StandardError
-        end
-      end
-
-      def parse(input)
-        @parser.parse(input)
-      end
-
-    end
-  end
-end
